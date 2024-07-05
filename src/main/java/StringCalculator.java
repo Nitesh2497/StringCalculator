c0864cbfd42da56c89da7b37412e3c4fee28e487
Nitesh Dewan <niteshdewan@Niteshs-MacBook-Air.local>
2024-07-05 21:31:16 +0530

    Added code for handling new line while adding of numbers

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -1,7 +1,7 @@
 
 public class StringCalculator {
 
-    public static int add(String numbers){
+    public int add(String numbers){
         if(numbers.length()<2) {
             if (numbers.isEmpty()) {
                 return 0;
@@ -9,7 +9,7 @@ public class StringCalculator {
                 return Integer.valueOf(numbers);
             }
         }else{
-            String[] number = numbers.split(",");
+            String[] number = numbers.split("[,\n]");
             int sum = 0;
             for (String num : number) {
                 sum += Integer.valueOf(num.trim());
